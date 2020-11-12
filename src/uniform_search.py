925ff9c07da1b6e93201617685e3f55045d3a81d
WillyNgo <ngo.willi@gmail.com>
2020-11-12 15:39:26 -0500

    project setup

---
+++ src/uniform_search.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 15:36:04 2020
+
+@author: NgoWi
+"""
+
---
+++ src/util.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 15:35:40 2020
+
+@author: NgoWi
+"""
+
