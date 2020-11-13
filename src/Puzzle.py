886de5223dc21c373dccf978907c6c0ccdb398e4
WillyNgo <ngo.willi@gmail.com>
2020-11-12 21:49:54 -0500

    fixed movement wrapping

--- src/Puzzle.py
+++ src/Puzzle.py
@@ -14,6 +14,10 @@ class Puzzle:
         self.height = height
         self.width = width
         self.cost = cost
+        self.upLeftCorner = 0
+        self.upRightCorner = self.width - 1
+        self.downLeftCorner = len(self.matrix) - (self.width)
+        self.downRightCorner = len(self.matrix) - 1
     
     def __swapNumbers(self, zero_index, move_index):
         temp = self.matrix[move_index]
@@ -25,67 +29,119 @@ class Puzzle:
         move_index = zero_index - self.width
         
         #swap the tiles
-        self.__swapNumbers(zero_index, move_index)
+        if move_index > 0:
+            self.__swapNumbers(zero_index, move_index)
+        else:
+            print("Cannot move up if zero is located on top row")
         
     def moveDown(self):
         zero_index = self.matrix.index(0)
         move_index = zero_index + self.width
         
         #swap the tiles
-        self.__swapNumbers(zero_index, move_index)
+        if move_index < len(self.matrix):
+            self.__swapNumbers(zero_index, move_index)
+        else:
+            print("Cannot move down if zero is located on bottom row")
         
-    def moveRight_regular(self):
+    def moveRight(self):
         zero_index = self.matrix.index(0)
-        move_index = zero_index + 1
+        move_index = -1
+        w = self.width
+        
+        #if the zero is located on a right corner
+        if zero_index == self.upRightCorner or zero_index == self.downRightCorner:
+            #wrap around and swap with left corner same row
+            move_index = zero_index - (w - 1)
+        else:
+            #else, move to the left like normal
+            move_index = zero_index + 1
+        
         
         #swap the tiles
         self.__swapNumbers(zero_index, move_index)
         
-    def moveLeft_regular(self):
+    def moveLeft(self):
         zero_index = self.matrix.index(0)
-        move_index = zero_index - 1
+        move_index = -1
+        w = self.width
         
-        #swap the tiles
-        self.__swapNumbers(zero_index, move_index)
-    
-    def moveRight_wrap(self):
-        zero_index = self.matrix.index(0)
-        move_index = zero_index - (self.width - 1)
-        
-        #swap the tiles
-        self.__swapNumbers(zero_index, move_index)
-        
-    def moveLeft_wrap(self):
-        zero_index = self.matrix.index(0)
-        move_index = zero_index + (self.width - 1)
+        #if the zero is located on a left corner
+        if zero_index == self.upLeftCorner or zero_index == self.downLeftCorner:
+            #wrap around and swap with right corner same row
+            move_index = zero_index + (w - 1)
+        else:
+            #else, move to the left like normal
+            move_index = zero_index - 1
         
         #swap the tiles
         self.__swapNumbers(zero_index, move_index)
         
+    #============ DIAGONAL MOVEMENTS ===============
     def moveUpRight(self):
         zero_index = self.matrix.index(0)
-        move_index = (zero_index - self.width) + 1
+        move_index = -1
+        w = self.width        
         
+        #if zero is located on down left corner, move diagonal
+        if zero_index == self.downLeftCorner:
+            move_index = zero_index - w + 1
+        #else if zero is located on down right corner, perform diagonal wrap
+        elif zero_index == self.downRightCorner:
+            move_index = zero_index - w - (w - 1)
+        else:
+            print("Can only perform an upward diagonal if zero is located on a bottom corner.")
+            
         #swap the tiles
         self.__swapNumbers(zero_index, move_index)
         
     def moveUpLeft(self):
         zero_index = self.matrix.index(0)
-        move_index = (zero_index - self.width) - 1
+        w = self.width
+        move_index = -1
+        
+        #if zero located on down right corner, move diagonal left
+        if zero_index == self.downRightCorner:
+            move_index = zero_index - w - 1
+        #else if zero is on down left corner, perform diagonal wrap
+        elif zero_index == self.downLeftCorner:
+            move_index = zero_index - w + (w - 1)
+        else:
+            print("Can only perform a upward diagonal if zero is located on bottom corner")
         
         #swap the tiles
         self.__swapNumbers(zero_index, move_index)
         
     def moveDownRight(self):
         zero_index = self.matrix.index(0)
-        move_index = (zero_index + self.width) + 1
+        move_index = -1
+        w = self.width        
         
+        #if zero is located on down left corner, move diagonal
+        if zero_index == self.upLeftCorner:
+            move_index = zero_index + (w + 1)
+        #else if zero is located on down right corner, perform diagonal wrap
+        elif zero_index == self.upRightCorner:
+            move_index = zero_index + w - (w - 1)
+        else:
+            print("Can only perform an downward diagonal if zero is located on a top corner.")
+            
         #swap the tiles
-        self.__swapNumbers(zero_index, move_index)  
+        self.__swapNumbers(zero_index, move_index) 
         
     def moveDownLeft(self):
         zero_index = self.matrix.index(0)
-        move_index = (zero_index + self.width) - 1
+        w = self.width
+        move_index = -1
+        
+        #if zero located on down right corner, move diagonal left
+        if zero_index == self.upRightCorner:
+            move_index = zero_index + w - 1
+        #else if zero is on down left corner, perform diagonal wrap
+        elif zero_index == self.upLeftCorner:
+            move_index = zero_index + w + (w - 1)
+        else:
+            print("Can only perform a downward diagonal if zero is located on top corner")
         
         #swap the tiles
         self.__swapNumbers(zero_index, move_index)  
--- src/test.py
+++ src/test.py
@@ -10,9 +10,18 @@ import util
 
 puzzle_arr = util.read_puzzle()
 puzzle_1 = puzzle_arr[1]
+puzzle_2 = [0,1,2,3,4,5,6,7,8] #3x3
+puzzle_3 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17] #3x6
 
-p = Puzzle(2, 4, puzzle_1, 0)
+p = Puzzle(3, 3, puzzle_2, 0)
 
 p.showState()
-p.moveRight_wrap()
+# p.moveDown()
+# p.moveUp()
+# p.moveLeft()
+# p.moveRight()
+# p.moveUpRight()
+# p.moveUpLeft()
+# p.moveDownRight()
+# p.moveDownLeft()
 p.showState()
