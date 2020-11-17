d0c43525aa7f26b7c7ac3001e6528ba818820439
shifatkhan <shifatkhan6@gmail.com>
2020-11-17 14:04:14 -0500

    Added checkgoal

--- src/a_star_search.py
+++ src/a_star_search.py
@@ -8,6 +8,3 @@ import util
 from Puzzle import Puzzle
 
 puzzle_arr = util.read_puzzle() #puzzle_arr contains all the initial puzzle in a list
-
-def checkGoal(puzzle):
-    
--- src/util.py
+++ src/util.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 12 15:35:40 2020
-
-@author: NgoWi
-"""
-
 puzzle_filepath = "../puzzles/initial_puzzle.txt"
 
 """
 This function reads the filepath containing the input file and returns
 a list containing each line of the input file as an array of ints
+
+Created on Thu Nov 12
+@author: WilliamNgo, ShifatKhan, Tahn
 """
 def read_puzzle():
     puzzle_arr = []
@@ -69,3 +66,26 @@ def find_successors(puzzle):
     if (p is not None): successors.append(p)
     
     return successors
+
+goal_puzzle1 = []
+
+"""
+Checks if the given puzzle 
+"""
+def checkGoal(puzzle):
+    length = len(puzzle.matrix)
+    width = puzzle.width
+    
+    goal = list(range(1, length+1))
+    goal[length-1] = 0
+    
+    goal2 = list(range(1, length+1))
+    index = 0
+    counter = 1
+    for i in range(1, length+1):
+        goal2[index] = i
+        index += width
+        if index >= length:
+            index = 0 + counter
+            counter += 1
+    goal2[length-1] = 0
