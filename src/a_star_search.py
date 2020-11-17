fcaced1e2f9ac752e32e1ce37e9fa3288d3e6809
shifatkhan <shifatkhan6@gmail.com>
2020-11-16 23:23:34 -0500

    changed comments

--- src/a_star_search.py
+++ src/a_star_search.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 12 15:36:14 2020
+Created on Thu Nov 16
 
-@author: NgoWi
+@author: ShifatKhan
 """
+import util
+from Puzzle import Puzzle
+
+def h0(puzzle):
+    if puzzle.matrix[len(puzzle.matrix) - 1] == 0:
+        return 0
+    else:
+        return 1
+
+puzzle_arr = util.read_puzzle()
+p1 = puzzle_arr[0]
+
+puzzle1 = Puzzle(2, 4, p1, 0)
+
+print(h0(puzzle1))
 
