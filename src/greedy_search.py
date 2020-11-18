eec028960a3b2ee8183a364108a324b2647e9a82
Akisukei <justin321261@hotmail.com>
2020-11-18 04:14:44 -0500

    fixed a problem in gbfs loop

--- src/greedy_search.py
+++ src/greedy_search.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import util
+import time
 from Puzzle import Puzzle
 from myQueue import myQueue
 
@@ -11,45 +12,51 @@ goal1, goal2 = util.createGoalStates(puzzle)
 def __remove_if_in_closed(success_list, closed_list):
     return [item for item in success_list if item not in closed_list]
 
-def run_with_h0(puzzle):
+def run_with_h0(root):
     
     # init vars
     open_list = myQueue()
     closed_list = []
     reach_goal = False
+    time_start = time.time()
+    time_limit = time_start + 60
     
-    # get h(n) of root and add it to closed list
-    puzzle.h1(goal1, goal2)
-    closed_list.insert(0, puzzle)
+    # get h(n) of root and add it to open list
+    root.h2(goal1, goal2)
+    open_list.push_sort_gbfs(root)
     
     while not reach_goal:
+        if time.time() > time_limit:
+            break
         
-        currentNode = closed_list[0]
+        currentNode = open_list.pop()
+        closed_list.append(currentNode)
         
         if util.checkIfGoalState(currentNode, goal1, goal2):
             reach_goal = True
-        else:
-            # find successors
-            successors = util.find_successors(puzzle)
-            successors = __remove_if_in_closed(successors, closed_list)
-            
-            # find best successor if have any
-            for s in successors:
-                s.h1(goal1, goal2)
-                # our priority queue will sort by h(n) and remove duplicates
-                open_list.push_sort_gbfs(s)
-               
-            # see if successor has lower h(n) than current node
-            if open_list._data:
-                bestSuccessor = open_list.pop()
-                        
-                if bestSuccessor.h_cost < currentNode.h_cost:
-                    closed_list.insert(0, bestSuccessor)
-                else:
-                    reach_goal = True  # nothing can beat current node
+            break
+        
+        # find successors
+        successors = util.find_successors(currentNode)
+        successors = __remove_if_in_closed(successors, closed_list)
+        
+        # find best successor if have any
+        for s in successors:
+            s.h2(goal1, goal2)
+            # our priority queue will sort by h(n) and remove duplicates
+            open_list.push_sort_gbfs(s)
+           
+        # see if successor has lower h(n) than current node
+        if open_list._data:
+            bestSuccessor = open_list.pop()
+                    
+            if bestSuccessor.h_cost < currentNode.h_cost:
+                closed_list.insert(0, bestSuccessor)
             else:
-                # no solution if open list is empty and goal is not reached
-                break
+                reach_goal = True  # nothing can beat current node
+        else:
+            # no solution if open list is empty and goal is not reached
+            break
     
     if not reach_goal:
         print("No solution found with GBFS")
