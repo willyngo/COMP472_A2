546d3797c697ed0b625ebd41a5592fb7295002da
Akisukei <justin321261@hotmail.com>
2020-11-18 04:47:00 -0500

    gbfs now can output

--- src/greedy_search.py
+++ src/greedy_search.py
@@ -4,15 +4,14 @@ import time
 from Puzzle import Puzzle
 from myQueue import myQueue
 
-puzzle_arr = util.read_puzzle() #puzzle_arr contains all the initial puzzle in a list
-puzzle = Puzzle(2, 4, puzzle_arr[0], 0, None)
-goal1, goal2 = util.createGoalStates(puzzle)
+puzzle_arr = util.read_puzzle()
+puzzle_50 = util.read_50_puzzles()
 
 
 def __remove_if_in_closed(success_list, closed_list):
     return [item for item in success_list if item not in closed_list]
 
-def run_with_h0(root):
+def run_gbfs(root, goal1, goal2, heuristic):
     
     # init vars
     open_list = myQueue()
@@ -22,7 +21,12 @@ def run_with_h0(root):
     time_limit = time_start + 60
     
     # get h(n) of root and add it to open list
-    root.h2(goal1, goal2)
+    if heuristic == 0:
+        root.h0()
+    elif heuristic == 1:
+        root.h1(goal1, goal2)
+    elif heuristic == 2:
+        root.h2(goal1, goal2)
     open_list.push_sort_gbfs(root)
     
     while not reach_goal:
@@ -42,7 +46,12 @@ def run_with_h0(root):
         
         # find best successor if have any
         for s in successors:
-            s.h2(goal1, goal2)
+            if heuristic == 0:
+                s.h0()
+            elif heuristic == 1:
+                s.h1(goal1, goal2)
+            elif heuristic == 2:
+                s.h2(goal1, goal2)
             # our priority queue will sort by h(n) and remove duplicates
             open_list.push_sort_gbfs(s)
            
@@ -62,17 +71,55 @@ def run_with_h0(root):
         print("No solution found with GBFS")
     else:
         # find solution path
-        for i in range(len(closed_list)):
-            print(closed_list[i].showState())
+        return closed_list, reach_goal, (time.time() - time_start)
         
 
-# end func
-
-
-def run_gbfs(puzzle):
+def run_50(heuristic):
+    total_solution_path = 0
+    total_search_path = 0
+    total_number_no_solution = 0
+    total_cost = 0
+    total_time = 0
     
-    run_with_h0(puzzle)
+    for i in range(50):
+        currPuzzle = Puzzle(2, 4, puzzle_50[i], 0, None)
+        
+        # Get goal states
+        goal1, goal2 = util.createGoalStates(currPuzzle)
+        
+        # Run GBFS
+        search_list, goalReached, time_taken = run_gbfs(currPuzzle, goal1, goal2, heuristic)
+
+        # Get Solution path
+        solution_list = util.create_solution_list(search_list[-1])
+        
+        # Calculate total values
+        total_solution_path += len(solution_list)
+        total_search_path += len(search_list)
+        total_number_no_solution += 1 if not goalReached else 0
+        total_cost += search_list[-1].g_cost
+        total_time += time_taken
     
+    util.output_analysis_50(total_solution_path, total_search_path, total_number_no_solution, total_cost, total_time, "gbfs", "h"+str(heuristic))
 
+def run(heuristic):
+    for i in range(3):
+        currPuzzle = Puzzle(2, 4, puzzle_arr[i], 0, None)
+        
+        # Get goal states
+        goal1, goal2 = util.createGoalStates(currPuzzle)
+        
+        # Run GBFS
+        search_list, goalReached, time_taken = run_gbfs(currPuzzle, goal1, goal2, heuristic)
 
-run_with_h0(puzzle)
+        # Get Solution path
+        solution_list = util.create_solution_list(search_list[-1])
+        
+        if goalReached:
+            util.output_solution_list(solution_list, i, time_taken, search_list[-1].g_cost, "gbfs", "h"+str(heuristic))
+            util.output_search_list(search_list, i, "gbfs", "h"+str(heuristic))
+        else:
+            util.output_no_solution(i, "gbfs", "h"+str(heuristic))
+
+run(1)
+run_50(1)
