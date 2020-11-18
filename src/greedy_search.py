1ac308d7667179d19f6d86a170dbc2ba24c61353
Akisukei <justin321261@hotmail.com>
2020-11-17 22:45:03 -0500

    updated gbfs loop

--- src/greedy_search.py
+++ src/greedy_search.py
@@ -1,49 +1,48 @@
 # -*- coding: utf-8 -*-
 import util
 from Puzzle import Puzzle
+from myQueue import myQueue
 
 try: import operator
 except ImportError: keyfun = lambda x: x.h_cost
 else: keyfun = operator.attrgetter("h_cost")
 
 puzzle_arr = util.read_puzzle() #puzzle_arr contains all the initial puzzle in a list
-
+goal1, goal2 = util.createGoalStates()
 
 def run_with_h0(puzzle):
     
     # init vars
-    open_list = []
+    open_list = myQueue()
     closed_list = []
-    reach_goal = False;
+    reach_goal = False
     
     # get h(n) of root and add it to closed list
     puzzle.h0()
     closed_list.insert(0, puzzle)
     
     while not reach_goal:
-        # find successors
-        currentNode = closed_list[0]
-        successors = util.find_successors(puzzle)
-        # find best successor
-        lowestCost = float("inf")
-        bestSucessor = None
         
-        for s in successors:
-            s.h0()
+        currentNode = closed_list[0]
+        
+        if util.checkIfGoalState(currentNode, goal1, goal2):
+            reach_goal = True
+        else:
+            # find successors
+            successors = util.find_successors(puzzle)
             
-            # check for dups in open list
-            for p in open_list:
-                if (s == p and s.h_cost < p.h_cost):
-                    open_list.remove(p)
-            open_list.append(s)
-            
-            # find lowest h(n)
-            if (s.h_cost < lowestCost):
-                bestSuccessor = s
-                lowestCost = s.h_cost
+            # find best successor
+            for s in successors:
+                s.h0()
+                # our priority queue will sort by h(n) and remove duplicates
+                open_list.push_sort_gbfs(s)
                 
-        if lowestCost >= closed_list[0].h_cost:
-            
+            bestSuccessor = open_list[0]
+                    
+            if bestSuccessor.h_cost < currentNode.h_cost:
+                closed_list.insert(0, bestSuccessor)
+            else:
+                reach_goal = True  # nothing can beat current node
         
         
     # add to list
